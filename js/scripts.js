09055cfae8950f00f3dcdb7fd1b13f5a844dd998
brazrei <79328469+brazrei@users.noreply.github.com>
2021-06-29 18:58:27 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function getAPIKEY(login, senha) {
   
  /* $.post("https://opmet.decea.mil.br/adm/login", { "username": login, "password": senha }, function( data ) {
     $( ".edtAPIKEY" ).html( data );
-  });  */
+  });  
   
   $.ajax({
   url: "https://opmet.decea.mil.br/adm/login",
@@ -26,6 +26,17 @@ function getAPIKEY(login, senha) {
   error: (function (erro) {
     console.log(erro)
   }) 
+})*/
+  $.ajax({
+        url: 'https://opmet.decea.mil.br/adm/login',
+        data: {"username":"priscila_bdc","password":"789Cimaer@"},
+        beforeSend: function(xhr) {
+             xhr.setRequestHeader("Content-Type", "application/json")
+        }, success: function(data){
+            alert(data);
+          console.log(data)
+            //process the JSON data etc
+        }
 })
   
 }
@@ -33,3 +44,8 @@ function getAPIKEY(login, senha) {
 function automatico () {
   //setTimeout(formataTAF, 1000);
 }
+/*
+curl -H 'Content-Type: application/json' -d '{"username":"priscila_bdc","password":"789Cimaer@"}' https://opmet.decea.mil.br/adm/login
+{"authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcmlzY2lsYV9iZGMiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJiZGMtc2VydmljZS5yZWFkIn0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifV0sInByb2ZpbGVSb2xlIjoiU1lTVEVNIiwiaWF0IjoxNjI0OTkyMTQ4LCJleHAiOjE2MjU4NTYxNDh9.H6ZHyd-aF0d0DAG5RoddaUt8q8L2NZe3PxtTpApNZro"}
+*/
+
