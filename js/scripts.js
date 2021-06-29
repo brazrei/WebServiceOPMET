65664ac9172d9d8c3ad23966b0d7fcbd3d581aa3
brazrei <79328469+brazrei@users.noreply.github.com>
2021-06-29 21:06:38 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,51 +7,23 @@ function loginOPMET() {
     let senha = $("#senha").val();
     
     getAPIKEY(addAspas (login), addAspas(senha)) ;
-  }
+}
+//"Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcmlzY2lsYV9iZGMiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJiZGMtc2VydmljZS5yZWFkIn0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifV0sInByb2ZpbGVSb2xlIjoiU1lTVEVNIiwiaWF0IjoxNjI1MDAwNjQyLCJleHAiOjE2MjU4NjQ2NDJ9.ZX421yQTBhFcZR1MK0CiSublkEN5MQstPzeddhGkMrU"
 
 function getAPIKEY(login, senha) {
-  
- /* $.post("https://opmet.decea.mil.br/adm/login", { "username": login, "password": senha }, function( data ) {
-    $( ".edtAPIKEY" ).html( data );
-  });  
-  
-  $.ajax({
-  url: "https://opmet.decea.mil.br/adm/login",
-  type: "post",
-  data: { "username": login, "password": senha },
+  const url = "https://opmet.decea.mil.br/adm/login";
 
-  success: function(data){ // trigger when request was successfull
-    $( ".edtAPIKEY" ).html( data );
-  },
-  error: (function (erro) {
-    console.log(erro)
-  }) 
-})
-  $.ajax({
-        url: 'https://opmet.decea.mil.br/adm/login',
-        data: {"username":"priscila_bdc","password":"789Cimaer@"},
-        beforeSend: function(xhr) {
-             xhr.setRequestHeader("Content-Type", "application/json")
-        }, success: function(data){
-            alert(data);
-          console.log(data)
-            //process the JSON data etc
-        }*/
-  
-  const url = "https://opmet.decea.mil.br/bdc/searchiepv?icaocodes=sbsp&begindate=2017102906&enddate=2017102907&format=JSON";
-
+  const data = {"username":"priscila_bdc","password":"789Cimaer@"}
   const options = {
+       method: 'POST',
        headers: {
-                 Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcmlzY2lsYV9iZGMiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJiZGMtc2VydmljZS5yZWFkIn0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifV0sInByb2ZpbGVSb2xlIjoiU1lTVEVNIiwiaWF0IjoxNjI0OTkyMTQ4LCJleHAiOjE2MjU4NTYxNDh9.H6ZHyd-aF0d0DAG5RoddaUt8q8L2NZe3PxtTpApNZro"
-              }
-    
+                 "Content-Type": "application/json"
+              },
+       body: JSON.stringify(data)
   };
-
   fetch(url, options)
     .then( res => res.json() )
     .then( data => console.log(data) );
-  
-  
 }
 
 function automatico () {
