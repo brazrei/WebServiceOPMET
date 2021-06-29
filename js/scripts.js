8798db845bd6d9c8a734d866e851bdb5561db1f6
brazrei <79328469+brazrei@users.noreply.github.com>
2021-06-29 18:12:08 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
-  function loginOPMET() {
+function addAspas(s) {
+  return '"' + s + '"'
+}
+
+function loginOPMET() {
     let login = $("#login").val();
     let senha = $("#senha").val();
     
-    getAPIKEY(login, senha) ;
+    getAPIKEY(addAspas (login), addAspas(senha)) ;
   }
 
 function getAPIKEY(login, senha) {
   
-  $.post("https://opmet.decea.mil.br/adm/login", { 'username': login, 'password': senha }, function( data ) {
+ /* $.post("https://opmet.decea.mil.br/adm/login", { "username": login, "password": senha }, function( data ) {
     $( ".edtAPIKEY" ).html( data );
-  });  
+  });  */
+  
+  $.ajax({
+  url: "https://opmet.decea.mil.br/adm/login",
+  type: "post",
+  data: { "username": login, "password": senha },
+
+  success: function(data){ // trigger when request was successfull
+    $( ".edtAPIKEY" ).html( data );
+  },
+  error: (function (erro) {
+    console.log(erro)
+  }) 
+})
+  
 }
 
 function automatico () {
