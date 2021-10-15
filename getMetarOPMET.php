e6a276a87696545474a9b0ff3298ade192d5c158
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 20:42:34 +0000

    Update getMetarOPMET.php

--- getMetarOPMET.php
+++ getMetarOPMET.php
@@ -1,13 +1,16 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  $PROXY_USER = "brazrab";    // Username
+  $PROXY_PASS = "12345678";   // Password
+  $PROXY_HOST = "proxy.decea.intraer"; // Proxy server address
+  $PROXY_PORT = "8080";
+  $token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmF6cmFiX3JkbXQiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifSx7ImF1dGhvcml0eSI6InJlZGVtZXQtc2VydmljZS5yZWFkIn1dLCJwcm9maWxlUm9sZSI6IlNZU1RFTSIsImlhdCI6MTYzNDMxNDUwNSwiZXhwIjoxNjM1MTc4NTA1fQ.bgmh6ehNESoxXOvaGjRLbiQfn-89mQvPZyisQNHKU3U";
     
-function setProxy() {
-    $PROXY_HOST = "proxy.decea.intraer"; // Proxy server address
+  function setProxyContext() {
     $PROXY_PORT = "8080";    // Proxy server port
-    $PROXY_USER = "brazrab";    // Username
-    $PROXY_PASS = "12345678";   // Password
     // Username and Password are required only if your proxy server needs basic authentication
     
     $auth = base64_encode("$PROXY_USER:$PROXY_PASS");
@@ -22,68 +25,36 @@ function setProxy() {
      )
     );
   }    
+
   function getMetar($access_token) {
-  $headers = array(
-    'Content-Type: application/json',
-    sprintf('Authorization: Bearer %s', $access_token)
-  );
+    $headers = array(
+      'Content-Type: application/json',
+      sprintf('Authorization: Bearer %s', $access_token)
+    );
 
-  $curl = curl_init("https://opmet.decea.mil.br/redemet/consulta_redemet?local=SBBH&msg=metar&data_ini=2021100100&data_fim=2021100101");
+    $curl = curl_init("https://opmet.decea.mil.br/redemet/consulta_redemet?local=SBBH&msg=metar&data_ini=2021100100&data_fim=2021100101");
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $result = json_decode(curl_exec($curl));
+    return $result;
+   }
 
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+   setProxyContext();
 
-  $result = json_decode(curl_exec($curl));
-
-  return $result;
-  }
-
-    setProxy();
-/*
-$url = 'http://dynupdate.no-ip.com/ip.php';
-$proxy = '127.0.0.1:8888';
-//$proxyauth = 'user:password';
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-$curl_scraped_page = curl_exec($ch);
-curl_close($ch);
-
-
-    echo $curl_scraped_page;
-    
-  */
-    /* API URL */
+   /* API URL */
     $url = 'https://opmet.decea.mil.br/redemet/consulta_redemet?local=SBBH&msg=metar&data_ini=2021100100&data_fim=2021100101';
-    $token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmF6cmFiX3JkbXQiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifSx7ImF1dGhvcml0eSI6InJlZGVtZXQtc2VydmljZS5yZWFkIn1dLCJwcm9maWxlUm9sZSI6IlNZU1RFTSIsImlhdCI6MTYzNDMxNDUwNSwiZXhwIjoxNjM1MTc4NTA1fQ.bgmh6ehNESoxXOvaGjRLbiQfn-89mQvPZyisQNHKU3U";
-    
-    //echo $token;
-    //exit;
-    //echo getMetar($token);
-    //exit;
        
     /* Init cURL resource */
     $ch = curl_init($url);
-        
-    /* Array Parameter Data */
-    //$data = ['local'=>'sbsp', 'msg'=>'metar', 'data_ini'=>'2021100100', 'data_fim'=>'2021100101'];
-        
-    /* pass encoded JSON string to the POST fields */
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        
+
     /* set the content type json */
     $headers = [];
     $headers[] = 'Content-Type:application/json';
     $headers[] = "Authorization: Bearer ".$token;
 
     //proxy settings
-    $proxy = 'proxy.decea.intraer:8080';
-    $proxyauth = 'brazrab:12345678';
+    $proxy = $PROXY_HOST  . ':' . $PROXY_PORT;
+    $proxyauth = '$PROXY_USER:$PROXY_PASS';
 
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
