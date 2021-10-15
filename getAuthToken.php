02ca1e7e5380679e880e6f96ac2723300818e204
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 21:27:56 +0000

    Update getAuthToken.php

--- getAuthToken.php
+++ getAuthToken.php
@@ -1,4 +1,6 @@
 <?php
+  include('proxy.php'); 
+
 /**
  * Get an authentication token
  */
@@ -26,15 +28,29 @@ function auth()
 {
    $serverURL = "https://opmet.decea.mil.br/adm/login";
    $cl = curl_init();
+
+    //proxy settings
+   $PROXY_USER = $GLOBALS['PROXY_USER'];
+   $PROXY_PASS = $GLOBALS['PROXY_PASS'];
+   $PROXY_HOST = $GLOBALS['PROXY_HOST'];
+   $PROXY_PORT = $GLOBALS['PROXY_PORT'];
+   $proxy = $PROXY_HOST  . ':' . $PROXY_PORT;
+   $proxyauth = $PROXY_USER . ":" . $PROXY_PASS;
+   curl_setopt($cl, CURLOPT_PROXY, $proxy);
+   curl_setopt($cl, CURLOPT_PROXYUSERPWD, $proxyauth);
+    //
+    
    curl_setopt($cl, CURLOPT_RETURNTRANSFER, true);
    curl_setopt($cl, CURLOPT_URL, "$serverURL");
    curl_setopt($cl, CURLOPT_POST, true);
-   /* uncomment this line if you don't have the required SSL certificates */
+
+    /* uncomment this line if you don't have the required SSL certificates */
    // curl_setopt($cl, CURLOPT_SSL_VERIFYPEER, false);
    curl_setopt($cl, CURLOPT_POSTFIELDS, array(
      "username" => "brazrab_rdmt",
      "password" => "00rEinaldo"
    ));
+    
    $auth_response = curl_exec($cl);
    if ($auth_response === false)
    {
