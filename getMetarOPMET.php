7f574f77d4662ffc041d76531fbdbb37db348f7c
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 18:18:57 +0000

    Update getMetarOPMET.php

--- getMetarOPMET.php
+++ getMetarOPMET.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     
 function setProxy() {
     $PROXY_HOST = "proxy.decea.intraer"; // Proxy server address
@@ -34,10 +37,10 @@ function setProxy() {
     //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         
     /* set the content type json */
-    $headers = [];
-    $headers[] = 'Content-Type:application/json';
+    //$headers = [];
+    //$headers[] = 'Content-Type:application/json';
     $token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmF6cmFiX3JkbXQiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifSx7ImF1dGhvcml0eSI6InJlZGVtZXQtc2VydmljZS5yZWFkIn1dLCJwcm9maWxlUm9sZSI6IlNZU1RFTSIsImlhdCI6MTYzNDMxNDUwNSwiZXhwIjoxNjM1MTc4NTA1fQ.bgmh6ehNESoxXOvaGjRLbiQfn-89mQvPZyisQNHKU3U";
-    $headers[] = "Authorization: Bearer ".$token;
+    $headers = ["Authorization: Bearer ".$token;]
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         
     /* set return type json */
