b0eea942fe9c2c58a6d1fea5042d5c91da2a33d7
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 19:53:34 +0000

    Update getAuthToken.php

--- getAuthToken.php
+++ getAuthToken.php
@@ -7,14 +7,13 @@ function auth()
    $serverURL = "https://opmet.decea.mil.br/adm/login";
    $cl = curl_init();
    curl_setopt($cl, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($cl, CURLOPT_URL, "$serverURL/oauth2/token");
+   curl_setopt($cl, CURLOPT_URL, "$serverURL");
    curl_setopt($cl, CURLOPT_POST, true);
    /* uncomment this line if you don't have the required SSL certificates */
    // curl_setopt($cl, CURLOPT_SSL_VERIFYPEER, false);
    curl_setopt($cl, CURLOPT_POSTFIELDS, array(
-     "grant_type" => "client_credentials",
-     "client_id" => "my@email.address",
-     "client_secret" => "my password"
+     "username" => "brazrab_rdmt",
+     "password" => "00rEinaldo"
    ));
    $auth_response = curl_exec($cl);
    if ($auth_response === false)
@@ -27,4 +26,5 @@ function auth()
    curl_close($cl);
    return json_decode($auth_response, true);
 }
+echo auth();
 ?>
