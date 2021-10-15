0493b1375839a1cb9c6c06d85549cf8644167e67
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 18:24:25 +0000

    Update getMetarOPMET.php

--- getMetarOPMET.php
+++ getMetarOPMET.php
@@ -37,10 +37,10 @@ function setProxy() {
     //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         
     /* set the content type json */
-    //$headers = [];
-    //$headers[] = 'Content-Type:application/json';
+    $headers = [];
+    $headers[] = 'Content-Type:application/json';
     $token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJicmF6cmFiX3JkbXQiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJhdWRpdC5jIn0seyJhdXRob3JpdHkiOiJhdWRpdC5kIn0seyJhdXRob3JpdHkiOiJhdWRpdC5yIn0seyJhdXRob3JpdHkiOiJhdWRpdC51In0seyJhdXRob3JpdHkiOiJjaGFuZ2UucGFzc3dvcmQifSx7ImF1dGhvcml0eSI6InJlZGVtZXQtc2VydmljZS5yZWFkIn1dLCJwcm9maWxlUm9sZSI6IlNZU1RFTSIsImlhdCI6MTYzNDMxNDUwNSwiZXhwIjoxNjM1MTc4NTA1fQ.bgmh6ehNESoxXOvaGjRLbiQfn-89mQvPZyisQNHKU3U";
-    $headers = ["Authorization: Bearer ".$token];
+    $headers[] = "Authorization: Bearer ".$token;
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         
     /* set return type json */
