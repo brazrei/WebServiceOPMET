1f516f7242b93b49386d21ef95964a3aa55ed728
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 21:11:54 +0000

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,6 @@
+<?php
+  $PROXY_USER = "brazrab";    // Username
+  $PROXY_PASS = "12345678";   // Password
+  $PROXY_HOST = "proxy.decea.intraer"; // Proxy server address
+  $PROXY_PORT = "8080";
+?>
